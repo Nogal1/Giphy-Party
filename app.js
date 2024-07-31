2d7ec17e8cc9cdab47b20674866aae13306484ad
Nogal1 <mcclendon.logan1234@gmail.com>
2024-07-30 18:32:29 -0700

    In a working state

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 console.log("Let's get this party started!");
+
+$(document).ready(function () {
+    const $gifContainer = $("#gif-container");
+    const $gifForm = $("#gif-form");
+    const $searchTerm = $("#search-term");
+
+    $gifForm.on("submit", async function (e) {
+        e.preventDefault();
+
+        let searchTerm = $searchTerm.val();
+        $searchTerm.val("");
+
+        const response = await axios.get("https://api.giphy.com/v1/gifs/search", {
+            params: {
+                q: searchTerm,
+                api_key: "V2jvQ966PCnskTU2cFwizguo781LKsIw", 
+            }
+        });
+
+        appendGif(response.data);
+    });
+
+    $("#remove-gifs").on("click", function () {
+        $gifContainer.empty();
+    });
+
+    function appendGif(res) {
+        if (res.data.length > 0) {
+            const gifUrl = res.data[0].images.original.url;
+            const $newGif = $("<img>", { src: gifUrl, alt: "GIF" });
+            $gifContainer.append($newGif);
+        }
+    }
+});
